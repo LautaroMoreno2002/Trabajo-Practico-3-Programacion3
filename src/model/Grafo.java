c309b77a81a386d3963cdde20f9f8fea5cfda877
Lautaro Moreno <lautymoreno80@gmail.com>
2023-10-13 23:05:59 -0300

    Creacion del proyecto

---
+++ src/model/Grafo.java
@@ -0,0 +1,77 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Grafo {
+	private int[][] _MA;
+	private ArrayList<HashSet<Integer>> _vecinos;
+	
+	public Grafo(int cantVertices) {
+		if (cantVertices < 0) 
+			throw new IllegalArgumentException("La cantidad de vértices para un grafo debe ser mayor o igual a cero");
+		_MA = new int[cantVertices][cantVertices];
+		_vecinos = new ArrayList<HashSet<Integer>>();
+		inicializarGrafo();
+	}
+	public void agregarArista(int verticeA,int verticeB) {
+		validarIndices(verticeA, verticeB);
+		_MA[verticeA][verticeB] = _MA[verticeB][verticeA] = 1;
+		_vecinos.get(verticeA).add(verticeB);
+		_vecinos.get(verticeB).add(verticeA);
+		System.out.println(imprimirMatriz());
+	}
+	public boolean existeArista(int verticeA,int verticeB) {
+		validarIndices(verticeA, verticeB);
+		return _MA[verticeA][verticeB] == 1;
+	}
+	public void borrarArista(int verticeA,int verticeB) {
+		validarIndices(verticeA, verticeB);
+		_MA[verticeA][verticeB] = _MA[verticeB][verticeA] = 0;
+		_vecinos.get(verticeA).remove(verticeB);
+		_vecinos.get(verticeB).remove(verticeA);
+		System.out.println(imprimirMatriz());
+	}
+	public HashSet<Integer> vecinoDelVertice(int vertice){
+		return _vecinos.get(vertice);
+	}
+		
+	private void validarIndices(int verticeA,int verticeB) {
+		if (verticeA == verticeB) 
+			throw new IllegalArgumentException("Los vértices deben ser distintos para no generar un loop");
+		if (verticeA < 0 || verticeB < 0) 
+			throw new IllegalArgumentException("Los vértices deben ser positivos y existir dentro del grafo");
+		else if (verticeA >= _MA.length || verticeB >= _MA.length) 
+			throw new IllegalArgumentException("Los vértices deben existir dentro del grafo");
+	}
+	private void inicializarGrafo() {
+		inicializarMAConCeroAristas();
+		inicializarVecinos();
+		System.out.println(imprimirMatriz());
+	}
+	private void inicializarMAConCeroAristas() {
+		for (int fila = 0; fila < _MA.length; fila++) {
+			for (int columna = 0; columna < _MA.length; columna++) {
+				_MA[fila][columna] = 0;
+			}
+		}
+	}
+	private void inicializarVecinos() {
+		for (int vertice = 0; vertice < _MA.length; vertice++) {
+			_vecinos.add(new HashSet<Integer>());
+		}
+	}
+	private String imprimirMatriz() {
+		StringBuilder MA = new StringBuilder();
+		MA.append("[");
+		for (int fila = 0; fila < _MA.length; fila++) {
+			for (int columna = 0; columna < _MA.length; columna++) {
+				MA.append(_MA[fila][columna]).append(", ");
+			}
+			MA.append("\n");
+		}
+		MA.setLength(MA.length()-2);
+		MA.append("]");
+		return MA.toString();
+	}
+}
---
+++ src/modelTest/GrafoTest.java
@@ -0,0 +1,54 @@
+package modelTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import model.Grafo;
+
+public class GrafoTest {
+	@Test (expected = IllegalArgumentException.class)
+	public void iniciarGrafoConCantidadVerticesNegativa() {
+		Grafo g = new Grafo(-1);
+		g.agregarArista(0, 2);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void agregarAristaLoop() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(0, 0);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void agregarAristaConPrimerVerticeExcedido() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(5, 0);
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void agregarAristaConSegundoVerticeExcedido() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(0, 5);
+	}
+	@Test
+	public void vertificarSiExisteAristaEnAmbosSentidos() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(1, 0);
+		assertTrue(g.existeArista(0, 1) && g.existeArista(0, 1));
+	}
+	@Test
+	public void eliminarArista() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(0, 2);
+		g.borrarArista(0, 2);
+		assertFalse(g.existeArista(0, 2));
+	}
+	@Test
+	public void vecinosDelVerticeUno() {
+		Grafo g = new Grafo(3);
+		g.agregarArista(1, 0);
+		g.agregarArista(1, 2);
+		g.agregarArista(0, 2);
+		String esperado = "[0, 2]";
+		String vecinosDelUno = g.vecinoDelVertice(1).toString();
+		assertEquals(esperado, vecinosDelUno);
+	}
+	
+}
