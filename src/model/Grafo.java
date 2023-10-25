059552f1d4718c486a3144a413a78f04e2ca4b3e
Lautaro Moreno <lautymoreno80@gmail.com>
2023-10-25 08:54:47 -0300

    Grafo sin MA, solo tiene vertices con vecinos y la cantidad de vertices

--- src/model/Grafo.java
+++ src/model/Grafo.java
@@ -10,15 +10,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Grafo {
-	private int[][] _MA;
-//	private ArrayList<HashSet<Integer>> _vecinos;
+	private int _cantVertices;
 	private ArrayList<Vertice> _verticesConVecinos;
 	
 	public Grafo(int cantVertices) {
 		if (cantVertices < 0) 
 			throw new IllegalArgumentException("La cantidad de vértices para un grafo debe ser mayor o igual a cero");
-		_MA = new int[cantVertices][cantVertices];
-//		_vecinos = new ArrayList<HashSet<Integer>>();
+		_cantVertices = cantVertices;
 		_verticesConVecinos = new ArrayList<Vertice>();
 		inicializarGrafo();
 	}
@@ -27,32 +25,31 @@ public class Grafo {
 	}
 	public void agregarArista(int verticeA,int verticeB) {
 		validarIndices(verticeA, verticeB);
-		_MA[verticeA][verticeB] = _MA[verticeB][verticeA] = 1;
-		//_vecinos.get(verticeA).add(verticeB);
-		//_vecinos.get(verticeB).add(verticeA);
 		agregarVecinosEnLosVertices(verticeA,verticeB);
 	}
 	public boolean existeArista(int verticeA,int verticeB) {
 		validarIndices(verticeA, verticeB);
-		return _MA[verticeA][verticeB] == 1;
+		boolean existeAristaAB = false;
+		boolean existeAristaBA = false;
+		for (Vertice v : _verticesConVecinos){
+				existeAristaAB |= (v.getIdVertice() == verticeA && v.getVecinos().contains(verticeB));
+			if (v.getIdVertice() == verticeB && v.getVecinos().contains(verticeA))
+				existeAristaBA |= (v.getIdVertice() == verticeB && v.getVecinos().contains(verticeA));
+		}
+		return (existeAristaAB && existeAristaBA);
 	}
 	public void borrarArista(int verticeA,int verticeB) {
 		validarIndices(verticeA, verticeB);
-		_MA[verticeA][verticeB] = _MA[verticeB][verticeA] = 0;
-//		_vecinos.get(verticeA).remove(verticeB);
-//		_vecinos.get(verticeB).remove(verticeA);
 		eliminarVecinoDeLosVertices(verticeA,verticeB);
-		System.out.println(imprimirMatriz());
 	}
 	public HashSet<Integer> vecinoDelVertice(int vertice){
-//		return _vecinos.get(vertice);
 		for (Vertice verticeActual : _verticesConVecinos)
 			if (verticeActual.getIdVertice() == vertice) 
 				return verticeActual.getVecinos();
 		return null;
 	}
 	public int cantidadVertices() {
-		return _MA.length;
+		return _cantVertices;
 	}
 		
 	private void validarIndices(int verticeA,int verticeB) {
@@ -60,32 +57,18 @@ public class Grafo {
 			throw new IllegalArgumentException("Los vértices deben ser distintos para no generar un loop");
 		if (verticeA < 0 || verticeB < 0) 
 			throw new IllegalArgumentException("Los vértices deben ser positivos y existir dentro del grafo");
-		else if (verticeA >= _MA.length || verticeB >= _MA.length) 
+		else if (verticeA >= _cantVertices || verticeB >= _cantVertices) 
 			throw new IllegalArgumentException("Los vértices deben existir dentro del grafo");
 	}
 	private void inicializarGrafo() {
-		inicializarMAConCeroAristas();
-//		inicializarVecinos();
 		inicializarVertices();
 	}
-	private void inicializarMAConCeroAristas() {
-		for (int fila = 0; fila < _MA.length; fila++) {
-			for (int columna = 0; columna < _MA.length; columna++) {
-				_MA[fila][columna] = 0;
-			}
-		}
-	}
 	private void inicializarVertices() {
-		for (int id = 0; id < _MA.length; id++) {
+		for (int id = 0; id < _cantVertices; id++) {
 			Vertice v = new Vertice(id);
 			_verticesConVecinos.add(v);
 		}
 	}
-//	private void inicializarVecinos() {
-//		for (int vertice = 0; vertice < _MA.length; vertice++) {
-//			_vecinos.add(new HashSet<Integer>());
-//		}
-//	}
 	private void agregarVecinosEnLosVertices(int verticeA, int verticeB) {
 		for (Vertice vertice : _verticesConVecinos) {
 			if (vertice.getIdVertice() == verticeA)
@@ -101,19 +84,6 @@ public class Grafo {
 			if (vertice.getIdVertice() == verticeB)
 				vertice.eliminarVecino(verticeA);
 		}	
-	}
-	private String imprimirMatriz() {
-		StringBuilder MA = new StringBuilder();
-		MA.append("[");
-		for (int fila = 0; fila < _MA.length; fila++) {
-			for (int columna = 0; columna < _MA.length; columna++) {
-				MA.append(_MA[fila][columna]).append(", ");
-			}
-			MA.append("\n");
-		}
-		MA.setLength(MA.length()-2);
-		MA.append("]");
-		return MA.toString();
 	}
 	
 	// Prueba
@@ -144,10 +114,6 @@ public class Grafo {
 		}
 		return g;
 	}
-
-//	public ArrayList<HashSet<Integer>> getVecinos() {
-//		return _vecinos;
-//	}
 	public ArrayList<Vertice> getVerticesConVecinos(){
 		return _verticesConVecinos;
 	}
